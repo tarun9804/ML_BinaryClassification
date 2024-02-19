9ce195c40bbdc3b9bdff4806d01387c41aa9f1b2
tarun9804 <tarun9804@gmail.com>
2024-02-19 18:26:16 +0530

    model trainer implemented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,13 +1,14 @@
 import os
 from dataclasses import dataclass
 from src.logger import logging
-
+import joblib
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 
 @dataclass
@@ -15,6 +16,7 @@ class DataingestionConfig:
     raw_data_path = "https://raw.githubusercontent.com/tarun9804/misc/main/Datasets/Classification/Indian%20Liver%20Patient%20Dataset%20(ILPD).csv"
     train_file_path = os.path.join("artefact", "test.csv")
     test_file_path = os.path.join("artefact", "train.csv")
+    preproc_obj_path = os.path.join("artefact","preprocobj.pkl")
 
 
 class DataIngestion:
@@ -26,21 +28,38 @@ class DataIngestion:
         logging.info("Processing started")
         df = pd.read_csv(self.ingestion_path_config.raw_data_path)
         logging.info("data downloaded")
-        num_col = df.select_dtypes(exclude='object').columns
-        cat_col = df.select_dtypes(include='object').columns
-        pp_obj = self.pre_proc_obj(num_col, cat_col);
-        x = pp_obj.fit_transform(df)
-        logging.info("Pre processing done")
-        df_n = pd.DataFrame(x, columns=cat_col.append(num_col))
-        train, test = train_test_split(df_n, test_size=0.2)
+
+        train, test = train_test_split(df, test_size=0.2)
         logging.info("split done")
         train.to_csv(self.ingestion_path_config.train_file_path, index=False)
         test.to_csv(self.ingestion_path_config.test_file_path, index=False)
         logging.info("train,test data saved, DONE")
 
+        target_train = train["Selector"]
+        train.drop(columns=["Selector"],inplace=True)
+        target_test = test["Selector"]
+        test.drop(columns=["Selector"],inplace=True)
+
+        num_col = train.select_dtypes(exclude='object').columns
+        cat_col = train.select_dtypes(include='object').columns
+        pp_obj = self.pre_proc_obj(num_col, cat_col);
+        logging.info("preproc object created")
+        x_train = pp_obj.fit_transform(train)
+        logging.info("train data transofrmed")
+        x_test = pp_obj.transform(test)
+        logging.info("test data transformed")
+        with open(self.ingestion_path_config.preproc_obj_path,"wb") as f:
+            joblib.dump(pp_obj, f)
+
+        logging.info("Pre processing obj saved")
+        return x_train, x_test, target_train, target_test
+
     def pre_proc_obj(self, num_col, cat_col):
-        num_pipeline = Pipeline([("impute1", SimpleImputer(strategy="mean"))])
-        cat_pipeline = Pipeline([("impute2", SimpleImputer(strategy="most_frequent"))])
+        num_pipeline = Pipeline([("impute1", SimpleImputer(strategy="mean")),
+                                 ("scaling",StandardScaler())])
+        cat_pipeline = Pipeline([("impute2", SimpleImputer(strategy="most_frequent")),
+                                 ("Onehot", OneHotEncoder()),
+                                 ("scaling",StandardScaler(with_mean=False))])
         ct = ColumnTransformer([
             ("cat_imputer", cat_pipeline, cat_col),
             ("num_imputer", num_pipeline, num_col)
---
+++ src/components/model_trainer.py
@@ -0,0 +1,92 @@
+from src.logger import logging
+from src.components.data_ingestion import DataIngestion
+import pandas as pd
+# models
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
+# metrics
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import ConfusionMatrixDisplay  # need to check
+from sklearn.model_selection import cross_val_score
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.models = {
+            "LogisticRegression": LogisticRegression(),
+            "RandomForestClassifier": RandomForestClassifier(),
+            "AdaBoostClassifier": AdaBoostClassifier(),
+            "GradientBoostingClassifier": GradientBoostingClassifier(),
+            "DecisionTreeClassifier": DecisionTreeClassifier(),
+            "SVC": SVC(),
+            "KNeighborsClassifier": KNeighborsClassifier()
+        }
+        self.params = {
+            "LogisticRegression": {},
+            "RandomForestClassifier": {'n_estimators': [8, 16, 32, 64, 128, 256]},
+            "AdaBoostClassifier": {'learning_rate': [.1, .01, .05, .001]},
+            "GradientBoostingClassifier": {  # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                'learning_rate': [.1, .01, .05, .001],
+                'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                # 'criterion':['squared_error', 'friedman_mse'],
+                # 'max_features':['auto','sqrt','log2'],
+                'n_estimators': [8, 16, 32, 64, 128, 256]},
+            "DecisionTreeClassifier": {'criterion': ['gini', 'entropy', 'log_loss'],
+                                       # 'splitter':['best','random'],
+                                       # 'max_features':['sqrt','log2'],
+                                       },
+            "SVC": {'C': [0.01, 0.1, 1, 10, 100], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid']},
+            "KNeighborsClassifier": {'leaf_size': list(range(1, 20)), 'n_neighbors': list(range(1, 20)), 'p': [1, 2]}
+
+        }
+
+    def run_model(self):
+        obj = DataIngestion()
+        x_train, x_test, y_train, y_test = obj.run_data_ingestion()
+        res = pd.DataFrame(columns=["model",
+                                    "train_accuracy_score",
+                                    "test_accuracy_score",
+                                    "train_roc_auc_score",
+                                    "test_roc_auc_score",
+                                    "tn",
+                                    "fp",
+                                    "fn",
+                                    "tp",
+                                    "tn-test",
+                                    "fp-test",
+                                    "fn-test",
+                                    "tp-test"])
+        j = 0;
+        for i in self.models:
+            model = self.models[i]
+            h_param = self.params[i]
+            print("running ", i)
+            gs = GridSearchCV(model, h_param, cv=3)
+            gs.fit(x_train, y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)
+            x_train_pred = model.predict(x_train)
+            x_test_pred = model.predict(x_test)
+            res.loc[j, "model"] = i
+            # scoring accuracy_score,roc_auc_score,confusion_matrix,cross_val_score
+            res.loc[j, "train_accuracy_score"] = accuracy_score(y_train, x_train_pred)
+            res.loc[j, "test_accuracy_score"] = accuracy_score(y_test, x_test_pred)
+            res.loc[j, "train_roc_auc_score"] = roc_auc_score(y_train, x_train_pred)
+            res.loc[j, "test_roc_auc_score"] = roc_auc_score(y_test, x_test_pred)
+            cm = confusion_matrix(y_train, x_train_pred)
+            res.loc[j, "tn"], res.loc[j, "fp"], res.loc[j, "fn"], res.loc[j, "tp"] = cm.flatten()
+            cm = confusion_matrix(y_test, x_test_pred)
+            res.loc[j, "tn-test"], res.loc[j, "fp-test"], res.loc[j, "fn-test"], res.loc[j, "tp-test"] = cm.flatten()
+            j = j + 1
+        print(res)
+
+
+if __name__ == "__main__":
+    obj = ModelTrainer()
+    obj.run_model()
